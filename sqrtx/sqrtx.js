a809df6b9e42ea5288f6698d6fc601347122baaa
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-02-23 14:56:19 -0500

    Time: 79 ms, Memory: 43.5 MB - LeetHub

--- sqrtx/sqrtx.js
+++ sqrtx/sqrtx.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-      if (x < 2) return x; // Base case
-  let left = 0, right = Math.floor(x/2) + 1; // Set search range
-  while (left < right) {
-    let mid = Math.floor((left + right) / 2);
-    if (mid * mid === x) return mid; // Found exact root
-    else if (mid * mid < x) left = mid + 1; // Search in right half
-    else right = mid; // Search in left half
-  }
-  return right - 1; // Return floor of square root   
+    if (x < 2) return x; // Base case
+    let left = 0, right = Math.floor(x/2) + 1; // Set search range
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (mid * mid === x) return mid; // Found exact root
+        else if (mid * mid < x) left = mid + 1; // Search in right half
+        else right = mid; // Search in left half
+      }
+    return right - 1; // Return floor of square root   
 };
