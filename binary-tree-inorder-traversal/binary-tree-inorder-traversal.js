7cfe8f4e71a80448dd4eae2b8281a589fa216731
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-02-24 00:51:15 -0500

    Time: 70 ms, Memory: 42.3 MB - LeetHub

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -10,17 +10,40 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
+//recursive
+// function inorderTraversal(root) {
+//   const result = [];
+//     //(root = F, [])
+//   traverse(root, result);
+//   return result;
+// }
+
+// function traverse(node, result) {
+//   if (node === null) {
+//     return;
+//   }
+//     //stay on left children until null,
+//   traverse(node.left, result);
+//   result.push(node.val);
+//   traverse(node.right, result);
+// }
+
+//iterative
 function inorderTraversal(root) {
   const result = [];
-  traverse(root, result);
-  return result;
-}
-
-function traverse(node, result) {
-  if (node === null) {
-    return;
+  const stack = [];
+  let curr = root;
+  
+  while (curr || stack.length > 0) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    
+    curr = stack.pop();
+    result.push(curr.val);
+    curr = curr.right;
   }
-  traverse(node.left, result);
-  result.push(node.val);
-  traverse(node.right, result);
+  
+  return result;
 }
