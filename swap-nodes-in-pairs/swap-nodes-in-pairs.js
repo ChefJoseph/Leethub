8e54d8155113cf8b8c348f41f3cb4c25ad0bdc1a
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-02-24 15:58:00 -0500

    Time: 63 ms, Memory: 41.9 MB - LeetHub

--- swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -9,6 +9,16 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+//visualization: 
+// Before swap:
+// 1 -> 2 -> 3 -> 4 -> null
+
+// After first swap:
+// 2 -> 1 -> 3 -> 4 -> null
+
+// After second swap:
+// 2 -> 1 -> 4 -> 3 -> null
+
 //recursive, S&T: O(n), visit each node once, and as deep as n
 // function swapPairs(head) {
 //   if (!head || !head.next) {
@@ -34,23 +44,28 @@ var swapPairs = function(head) {
     if (!head || !head.next) {
         return head;
     }
-    
+    // Create a dummy node to serve as the new head
     let dummyHead = new ListNode(0);
+    // Point the dummy node to the original head
     dummyHead.next = head;
+    // Create a pointer to the dummy node
     let prev = dummyHead;
     
+    // Loop through the linked list, swapping adjacent nodes
     while (head && head.next) {
+        // Create pointers to the two nodes to be swapped
         let first = head;
         let second = head.next;
+        // swap
         let next = second.next;
-        
         prev.next = second;
+        
         second.next = first;
         first.next = next;
         
         prev = first;
         head = next;
     }
-    
+    // Return the new head (which is the dummy node's next node)
     return dummyHead.next;
 };
