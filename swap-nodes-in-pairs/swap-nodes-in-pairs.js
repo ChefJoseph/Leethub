e1be18b6ee3ce314c736edf49cbc8b433f9c0f2f
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-02-24 15:50:11 -0500

    Time: 60 ms, Memory: 42.3 MB - LeetHub

--- swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -10,20 +10,47 @@
  * @return {ListNode}
  */
 //recursive, S&T: O(n), visit each node once, and as deep as n
-function swapPairs(head) {
-  if (!head || !head.next) {
-    // base case: empty list or only one node
-    return head;
-  }
+// function swapPairs(head) {
+//   if (!head || !head.next) {
+//     // base case: empty list or only one node
+//     return head;
+//   }
   
-  // save the next node to use in the recursion
-  const nextPair = head.next.next;
+//   // save the next node to use in the recursion
+//   const nextPair = head.next.next;
   
-  // swap the adjacent nodes
-  const newHead = head.next;
-  newHead.next = head;
-  head.next = swapPairs(nextPair);
+//   // swap the adjacent nodes
+//   const newHead = head.next;
+//   newHead.next = head;
+//   head.next = swapPairs(nextPair);
   
-  // return the new head of the swapped pair
-  return newHead;
-}
+//   // return the new head of the swapped pair
+//   return newHead;
+// }
+
+//iteratively S&T: 
+
+var swapPairs = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    
+    let dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    let prev = dummyHead;
+    
+    while (head && head.next) {
+        let first = head;
+        let second = head.next;
+        let next = second.next;
+        
+        prev.next = second;
+        second.next = first;
+        first.next = next;
+        
+        prev = first;
+        head = next;
+    }
+    
+    return dummyHead.next;
+};
