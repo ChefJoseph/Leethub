3139e45f22e4b8d8ff5281cfadcba74e31c91e3e
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-05-25 14:23:23 -0400

    Time: 67 ms, Memory: 42.5 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.js
@@ -8,9 +8,9 @@ function maxProfit(prices) {
     let profit = 0;
 //iterate throught prices array
     
-//if it is profitable, then add to profit
+//if it is profitable (the next day is more than previous), then add diff to profit
 //because the stock can be bought and sold same day,
-//the current day and previous day is profit is recorded
+//the current day and previous day profit is recorded
     for(let i = 0; i < prices.length; i++){
         if(prices[i] - prices[i-1] > 0){
             profit += prices[i] - prices[i-1]
