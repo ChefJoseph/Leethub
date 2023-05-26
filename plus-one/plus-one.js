a09a44a1a60a128ae4e79691a69de3061acf679c
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-05-26 14:54:04 -0400

    Time: 64 ms, Memory: 42.4 MB - LeetHub

--- plus-one/plus-one.js
+++ plus-one/plus-one.js
@@ -2,7 +2,7 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {   
+// var plusOne = function(digits) {   
 //     //brute force:    
 //         let joined = BigInt(digits.join(""));
 //         joined++;
@@ -11,18 +11,27 @@ var plusOne = function(digits) {
     
 // }
 
-//iterate from right to left
-    for (let i = digits.length - 1; i>= 0; i--) {
-//no carrying 
-        if (digits[i] < 9) {
-            digits[i]++;
-            return digits; //ends loop
-            
-        } else {
-            digits[i] = 0;
+function plusOne(digits) {
+    //init carry to incr by 1
+    let carry = 1;
+    //iter starting with last digit until index of 0
+    for (let i = digits.length - 1; i >= 0; i--) {
+        let digit = digits[i];
+        //Incr 1
+        let sum = digit + carry;
+        //If digit not adding 10, then carrying 0
+        carry = Math.floor(sum / 10);
+        //if under 10, leave as is, if >=10, then leave the single digit.
+        digits[i] = sum % 10;
+        // not carrying, break. The incr 1 is complete
+        // or else carry = 1 continues in loop
+        if (carry === 0) {
+          break;
         }
     }
-//if digits are zeros add 1 to front
-    digits.unshift(1);
+    //in the case of digits = [0,0,0], add 1 to beginning
+    if (carry === 1) {
+        digits.unshift(1);
+    }
     return digits;
-};
+}
