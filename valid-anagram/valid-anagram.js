d506e4ad99590f4b15441abcc161cb4964c1d69b
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-06-14 15:32:40 -0400

    Time: 79 ms, Memory: 42.3 MB - LeetHub

--- valid-anagram/valid-anagram.js
+++ valid-anagram/valid-anagram.js
@@ -10,31 +10,54 @@
 //1)Sort O(nlogn)
 //2)Check if length matches
 //2)Check if letters match
-function isAnagram(s, t) {
-    if (s.length !== t.length) {
+// function isAnagram(s, t) {
+//     //if not same length, return false
+//     if (s.length !== t.length) {
+//         return false;
+//     }
+//     //init hash object
+//     const count = {};
+//     // Count the frequency of characters in s
+//     for (let i = 0; i < s.length; i++) {
+//         count[s[i]] = (count[s[i]] || 0) + 1;
+//     }
+//     //Decremenet the frequency of t[i] in count
+//     for (let i = 0; i < s.length; i++) {
+//         if (!count[t[i]]){
+//             return false;
+//         }
+//         count[t[i]]--;
+//     }
+//     // All char should be zero. 
+//     for(const char in count) {
+//         if(count[char] !== 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+function isAnagram(s,t) {
+    if(s.length !== t.length) {
         return false;
     }
-    const count = {};
-    // Count the frequency of characters in s
-    for (let i = 0; i < s.length; i++) {
+    let count = {};
+    for(let i = 0; i < s.length; i++){
         count[s[i]] = (count[s[i]] || 0) + 1;
     }
-    //Decremenet the frequence of t[i] in count
-    for (let i = 0; i < s.length; i++) {
-        if (!count[t[i]]){
+    for(let i = 0; i < s.length; i++){
+        if(!count[t[i]]){
             return false;
         }
         count[t[i]]--;
     }
-    //Check if all frequencies are zero. Equal count if anagram.
-    for(const char in count) {
+    for(let char in count){
         if(count[char] !== 0) {
             return false;
         }
     }
     return true;
 }
-    
 
 // var isAnagram = function(s, t) {
 // //turn each character into substring and sort alphabetically
