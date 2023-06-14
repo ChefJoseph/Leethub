e53109a95649906f03b49ada590d57321567fe2e
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-06-14 15:12:19 -0400

    Time: 82 ms, Memory: 42.1 MB - LeetHub

---
+++ valid-anagram/valid-anagram.js
@@ -0,0 +1,57 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+// Solutions#1
+// T O(n)
+// S O(n)
+
+//1)Sort O(nlogn)
+//2)Check if length matches
+//2)Check if letters match
+function isAnagram(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    const count = {};
+    // Count the frequency of characters in s
+    for (let i = 0; i < s.length; i++) {
+        count[s[i]] = (count[s[i]] || 0) + 1;
+    }
+    //Decremenet the frequence of t[i] in count
+    for (let i = 0; i < s.length; i++) {
+        if (!count[t[i]]){
+            return false;
+        }
+        count[t[i]]--;
+    }
+    //Check if all frequencies are zero. Equal count if anagram.
+    for(const char in count) {
+        if(count[char] !== 0) {
+            return false;
+        }
+    }
+    return true;
+}
+    
+
+// var isAnagram = function(s, t) {
+// //turn each character into substring and sort alphabetically
+//     let sSort = s.split("").sort();  
+//     let tSort = t.split("").sort();
+//     console.log({sSort,tSort})
+//  //return false if both strings are not equal length   
+//     if (sSort.length !== tSort.length) {
+//         return false
+//     }
+// //iterate through strings and if characters do not match at each index, return false
+//     for (let i = 0; i < s.length; i++) {
+//         if(sSort[i] !== tSort[i]) {
+//             return false
+//         }
+//     }
+// //return true if they are equal length and have matching characters
+//     return true
+// };
+
