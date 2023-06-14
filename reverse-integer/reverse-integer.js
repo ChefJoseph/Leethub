9f9b98d49982976975174f01b576283102c577ff
Joseph Hirotsu <105380044+ChefJoseph@users.noreply.github.com>
2023-06-14 14:46:53 -0400

    Time: 82 ms, Memory: 44 MB - LeetHub

--- reverse-integer/reverse-integer.js
+++ reverse-integer/reverse-integer.js
@@ -2,28 +2,23 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    //turn integer into string
-    //divide string into substrings
-    //reverse string
-    //combines substrings into one new string
-    var toStr = x.toString().split("").reverse().join("");
-    
-    // console.log(toStr, "toStr")
-    // console.log(toStr.split(""), "split")
-    //parses string into integer
-    //2,147,483,647 is 32-bit unsigned integer
-        if (parseInt(toStr) > 2147483647) return 0;
-    //if last character is -, use as seprator
-    //contact '-' and first substring 
-    //parse string into integer
-        if (toStr.substring(toStr.length - 1) === '-') {
-            const newStr = toStr.split('-');
-            // console.log(newStr, "newStr")
-            const negStr = '-' + newStr[0];
-            return parseInt(negStr);
+
+function reverse(x) {
+    let reversed = 0;
+    const limit = Math.pow(2, 31);
+      while (x !== 0) {
+        const digit = x % 10;
+
+        // Check if reversing x would cause it to go outside the 32-bit range
+        if (reversed > Math.floor(limit / 10) || (reversed === Math.floor(limit / 10) && digit > 7)) {
+            return 0;
         }
+        if (reversed < Math.ceil(-limit / 10) || (reversed === Math.ceil(-limit / 10) && digit < -8)) {
+            return 0;
+        }
+        reversed = reversed * 10 + digit;
+        x = Math.trunc(x / 10);
+      }
 
-    return parseInt(toStr);
+  return reversed;
 }
-
